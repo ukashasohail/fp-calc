a07e918046091a5ca2efaa1647f55502ab676179
ukasha <ukasha909@outlook.com>
2022-02-03 21:13:50 +0500

    new UI

--- script.py
+++ script.py
@@ -3,10 +3,10 @@ f = open("./data.txt", "r")
 data = f.read()
 soup = BeautifulSoup(data, "html.parser")
 prices = []
-raw = soup.find_all("span", class_="total")
+raw = soup.find_all("div", class_="item-price")
 
 for i in raw:
-    prices.append(float(i.string.strip()[2:].replace(',','')))
+    prices.append(float(i.string.rstrip("\n")[3:].replace(',','')))
 
 print("total price:", sum(prices))
 print("total orders: ",len(prices))
